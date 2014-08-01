f78571f9a7f51da94a1f0245148305b1c41a1839
Francesc <fpbosch@gmail.com>
2014-08-01 18:39:52 +0200

    Added bootstrap, jquery and fontawesome

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -5,12 +5,17 @@ require.config({
 		'domReady': '../app/lib/requirejs-domready/domReady',
 		'angular': '../app/lib/angular/angular',
 		'angular.route': '../app/lib/angular-route/angular-route',
+		'jquery': '..app/lib/jquery/dist/jquery',
+		'getbootstrap': '..app/lib/bootstrap/dist/js/bootstrap',
+ 
 	},// angular does not support AMD out of the box, put it in a shim
 	shim: {
 		'angular': {
 			exports: 'angular'
 		},
-		'angular.route': ['angular']
+		'angular.route': ['angular'],
+		'getbootstrap': ['jquery']
+
 	},
 	// kick start application
 	deps: ['./bootstrap']
