f327e900c5dba1b9666ba8738e7fdaac632fbde3
Francesc <fpbosch@gmail.com>
2014-08-15 20:23:55 +0200

    Add more ui options for the page edit

--- assets/js/app/controllers/pages-edit-ctrl.js
+++ assets/js/app/controllers/pages-edit-ctrl.js
@@ -2,6 +2,10 @@ define(['./module'], function (controllers) {
 	'use strict';
   controllers.controller('PagesEditCtrl', ['$scope', '$routeParams', 'pagesService', function ($scope, $routeParams, itemService) {
 
+  	$scope.title='';
+  	$scope.body='';
+  	$scope.order=0;
+  	
   	var result = itemService.getOne($routeParams.itemId).success(function(data) {
 		//console.log(pages);
 		console.log(data);
@@ -10,9 +14,9 @@ define(['./module'], function (controllers) {
 		alert('Houston, we got a problem!');
 	});
 
-	$scope.ok = function(id, title, body) {
-  		console.log('El body: '+body);
-		var result = itemService.update(id, title, body).success(function(item) {
+	$scope.ok = function(id, title, body, order) {
+  		console.log('El body: '+body+' order '+order);
+		var result = itemService.update(id, title, body, order).success(function(item) {
 			$scope.result = item;
 		}).error(function (data) {
 			alert('Houston, we got a problem!');
