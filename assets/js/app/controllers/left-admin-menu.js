51d61f7ff496f0396f9658eddaf1373242a59dc6
Francesc <fpbosch@gmail.com>
2014-08-15 20:22:17 +0200

    Adapted display style to angularjs for pages and post left side menu

--- assets/js/app/controllers/left-admin-menu.js
+++ assets/js/app/controllers/left-admin-menu.js
@@ -1,5 +1,28 @@
 define(['./module'], function (controllers) {
 	'use strict';
-  controllers.controller('LeftAdminMenu', [function ($scope) {}]);
+  controllers.controller('LeftAdminMenu', ['$scope', function ($scope) {
+
+  	$scope.showPostOptions='none';
+  	$scope.showPageOptions='none';
+
+	$scope.show_post_options = function() {
+
+	  	if ($scope.showPostOptions=='none') 
+	  		$scope.showPostOptions='block';
+	  	else
+	  		$scope.showPostOptions='none';
+
+	};
+
+	$scope.show_page_options = function() {
+
+	  	if ($scope.showPageOptions=='none') 
+	  		$scope.showPageOptions='block';
+	  	else
+	  		$scope.showPageOptions='none';
+
+	};
+
+  }]);
 
 });
